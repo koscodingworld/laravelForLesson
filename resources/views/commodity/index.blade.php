b536253a040ac39cfc8f863268d887687bf527b9
royyy <roy0072809@gmail.com>
2019-08-10 23:10:15 +0800

    1. 顯示商品名稱及售價

--- resources/views/commodity/index.blade.php
+++ resources/views/commodity/index.blade.php
@@ -4,7 +4,10 @@
 <div class="container-fluid">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            
+            @foreach ($commodities as $commodity)
+               <p>名稱：{{ $commodity->name }}</p>
+               <p>售價：{{ $commodity->price }}</p>
+            @endforeach
         </div>
     </div>
 </div>
